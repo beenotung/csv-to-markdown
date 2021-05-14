5ade68dc877f36e49636891272f9ab018f083633
Beeno Tung <aabbcc1241@yahoo.com.hk>
2021-05-14 15:21:32 +0800

    show hint to stderr instead of stdout
    to allow pipeing the result for further usage

--- csv-to-markdown.ts
+++ csv-to-markdown.ts
@@ -4,7 +4,7 @@ import { from_csv, csv_to_table_text } from '@beenotung/tslib/csv'
 
 let input = process.argv[2]
 if (!input) {
-  console.log('reading from stdin')
+  console.error('reading from stdin...')
   input = '/dev/stdin'
 }
 let text = readFileSync(input).toString()
