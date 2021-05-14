aeb6a3d3528ed170f59cbd5215f6c03937939725
Beeno Tung <aabbcc1241@yahoo.com.hk>
2021-05-14 15:01:51 +0800

    implemented cli tools to convert csv and tsv to markdown table

---
+++ csv-to-markdown.ts
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+import { readFileSync } from 'fs'
+import { from_csv, csv_to_table_text } from '@beenotung/tslib/csv'
+
+let input = process.argv[2]
+if (!input) {
+  console.log('reading from stdin')
+  input = '/dev/stdin'
+}
+let text = readFileSync(input).toString()
+let rows = from_csv(text)
+text = csv_to_table_text(rows, { markdown: true })
+console.log(text)
